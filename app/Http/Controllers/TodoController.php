d118518f177c5da8da531f309fa1d53cb81d3335
Jeremy <jeremy.lamit@zenrooms.com>
2018-05-16 18:12:03 +0700

    initial commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function index()
+    {
+        return response(Todo::latest()->get(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'text' => 'required',
+            'finished' => 'required|boolean',
+        ]);
+
+        $todo = Todo::create($data);
+
+        return response($todo, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response($todo, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $data = $request->validate([
+            'text' => 'required',
+            'finished' => 'required|boolean',
+        ]);
+
+        $todo->update($data);
+
+        return response($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo $todo
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response('Deleted Succesfully', 200);
+    }
+}
